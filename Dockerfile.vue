b77eecca8241fd2530db9ca7f2b3692bd5c02219
Jatin Goel <jatingoel1037@hotmail.com>
2020-07-29 22:58:53 +0530

    Add Auth0 to Vue app

--- Dockerfile.vue
+++ Dockerfile.vue
@@ -8,7 +8,8 @@ COPY src/vue/frontend/package*.json ./
 
 RUN set -eux; \
     npm install; \
-    npm install -g http-server
+    npm install -g http-server; \
+    npm install --save auth0-js eventemitter3 axios
 
 COPY src/vue/frontend ./
 
